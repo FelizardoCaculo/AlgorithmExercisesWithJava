9eab8a11b198c8379b0fc218e4931e102403679c
FelizardoCaculo <felizardo1987@gmail.com>
2024-07-07 16:34:35 +0100

    Add Exercise 001

--- AlgorithmExercisesWithJava/src/Exercise001.java
+++ AlgorithmExercisesWithJava/src/Exercise001.java
@@ -1,5 +1,6 @@
 public class Exercise001 {
     public static void main(String[] args) {
-        
+        //001 - Crie um programa que escreva "Olá mundo" na tela
+        System.out.println("\nOlá mundo!\n");
     }
 }
